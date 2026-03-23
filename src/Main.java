d8343bea2bdacb31613940d2700de61f85d740a5
christopher18-cyber <christopheradegoke4@gmail.com>
2026-03-23 03:23:44 +0100

    dealt more with scanner and did mad libs game and the dealt with arithmetic operators

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+//
+//public class Main {
+//    public static void main(String[] args){
 //        System.out.print("Enter your name: ");
 //        String name = scanner.nextLine();
 //        System.out.print("Enter your age: ");
@@ -17,17 +16,89 @@ public class Main {
 //            System.out.println("You are enrolled as a student.");
 //        }else{
 //            System.out.println("You are NOT enrolled as a student.");
+//
+//        System.out.print("Enter your age: ");
+//        int age= scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.print("Enter your name: ");
+//        String name= scanner.nextLine();
+//
+//        System.out.println("You are "+name+ " and you are "+age+ " years old");
+//
+//
+//        double width =0;
+//        double height=0;
+//        double area=0;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the width: ");
+//        width= scanner.nextDouble();
+//        scanner.nextLine();
+//        System.out.print("Enter the height: ");
+//        height=scanner.nextDouble();
+//        scanner.nextLine();
+//
+//        area=height*width;
+//
+//        System.out.println("The area is "+area);
+//        scanner.close();
+//
+//
+//    }
+//}
+// }
 
-        System.out.print("Enter your age: ");
-        int age= scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.print("Enter your name: ");
-        String name= scanner.nextLine();
+//public class Main {
+//    public static void main(String[] args){
+//
+////        MAD LIBS GAME
+//
+//        Scanner scanner= new Scanner(System.in);
+//
+//        String adjective1;
+//        String noun1;
+//        String adjective2;
+//        String verb1;
+//        String adjective3;
+//
+//        System.out.print("Enter an adjective (description): ");
+//        adjective1=scanner.nextLine();
+//        System.out.print("Enter a noun (animal or person): ");
+//        noun1=scanner.nextLine();
+//        System.out.print("Enter an adjective (description): ");
+//        adjective2=scanner.nextLine();
+//        System.out.print("Enter a verb ending with -ing (action): ");
+//        verb1=scanner.nextLine();
+//        System.out.print("Enter an adjective (description): ");
+//        adjective3=scanner.nextLine();
+//
+//        System.out.println("Today I went to a "+ adjective1+ " zoo.");
+//        System.out.println("In an exhibit, I saw a "+ noun1 + ".");
+//        System.out.println(noun1 + " was "+ adjective2+ " and " +verb1+ "!");
+//        System.out.println("I was "+ adjective3+ "!");
+//    }
+//}
 
-        System.out.println("You are "+name+ " and you are "+age+ " years old");
-        scanner.close();
 
+public class Main {
+    public static void main(String[] args){
 
+//        Arithmetic operators
+//        int x=10;
+//        int y=2;
+//
+//        int z;
+//        z=x+y;
+
+//        Augmented assignment operators
+
+        int x=10;
+        int y= 3;
+
+        x +=y;
+
+        System.out.println(x);
     }
 }
