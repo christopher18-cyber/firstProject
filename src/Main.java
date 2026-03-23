a3a04ea36dcf1898dfe88a8b730b1dea1e693d0c
christopher18-cyber <christopheradegoke4@gmail.com>
2026-03-23 01:46:14 +0100

    scanner

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age= scanner.nextInt();
+        System.out.print("Enter your gpa: ");
+        double gpa= scanner.nextDouble();
+        System.out.println("Your name is "+name+ " and you are "
+                +age+" years old with a gpa of " +gpa);
+        scanner.close();
+
+
+    }
+}
