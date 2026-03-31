8cb7c6b3960b1147793558fbeaac735410a50526
christopher18-cyber <christopheradegoke4@gmail.com>
2026-03-31 04:45:45 +0100

    learnt a java slot machine, paper rock machine, java quiz, 2D array, and searching through an array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 //
 //public class Main {
 //    public static void main(String[] args){
@@ -880,93 +882,439 @@ import java.util.Random;
 //    }
 //}
 
-public class Main {
-    public static void main(String[] args){
+//public class Main {
+//    public static void main(String[] args){
 //        JAVA DICE ROLLER PROGRAM
-
-        Scanner scanner= new Scanner(System.in);
-        Random random = new Random();
-        int numOfDice;
-        int total=0;
-
-        System.out.print("Enter the number of dice to roll: ");
-        numOfDice=scanner.nextInt();
-
-        if(numOfDice > 0){
-
-            for(int i=0; i< numOfDice; i++){
-                int roll = random.nextInt(1,7);
-                printDie(roll);
-                System.out.println("You rolled "+ roll);
-                total = total + roll;
-            }
-            System.out.println("Total: "+ total);
-        }else{
-            System.out.println("The number of dice must be greater than 0");
-        }
-
-        scanner.close();
-    }
-
-    static void printDie(int roll){
-        String dice1 = """
-                 -------
-                |       |
-                |   •   |
-                |       |
-                 -------
-                """;
-
-
-        String dice2 = """
-                 -------
-                | •     |
-                |       |
-                |     • |
-                 -------
-                """;
-
-        String dice3 = """
-                 -------
-                | •     |
-                |   •   |
-                |     • |
-                 -------
-                """;
-
-        String dice4 = """
-                 -------
-                | •   • |
-                |       |
-                | •   • |
-                 -------
-                """;
-
-        String dice5 = """
-                 -------
-                | •   • |
-                |   •   |
-                | •   • |
-                 -------
-                """;
-
-        String dice6 = """
-                 -------
-                | •   • |
-                | •   • |
-                | •   • |
-                 -------
-                """;
-
-        switch (roll){
-            case 1 -> System.out.println(dice1);
-            case 2 -> System.out.println(dice2);
-            case 3 -> System.out.println(dice3);
-            case 4 -> System.out.println(dice4);
-            case 5 -> System.out.println(dice5);
-            case 6 -> System.out.println(dice6);
-            default -> System.out.println("Invalid roll");
+//
+//        Scanner scanner= new Scanner(System.in);
+//        Random random = new Random();
+//        int numOfDice;
+//        int total=0;
+//
+//        System.out.print("Enter the number of dice to roll: ");
+//        numOfDice=scanner.nextInt();
+//
+//        if(numOfDice > 0){
+//
+//            for(int i=0; i< numOfDice; i++){
+//                int roll = random.nextInt(1,7);
+//                printDie(roll);
+//                System.out.println("You rolled "+ roll);
+//                total = total + roll;
+//            }
+//            System.out.println("Total: "+ total);
+//        }else{
+//            System.out.println("The number of dice must be greater than 0");
+//        }
+//
+//        scanner.close();
+//    }
+//
+//    static void printDie(int roll){
+//        String dice1 = """
+//                 -------
+//                |       |
+//                |   •   |
+//                |       |
+//                 -------
+//                """;
+//
+//
+//        String dice2 = """
+//                 -------
+//                | •     |
+//                |       |
+//                |     • |
+//                 -------
+//                """;
+//
+//        String dice3 = """
+//                 -------
+//                | •     |
+//                |   •   |
+//                |     • |
+//                 -------
+//                """;
+//
+//        String dice4 = """
+//                 -------
+//                | •   • |
+//                |       |
+//                | •   • |
+//                 -------
+//                """;
+//
+//        String dice5 = """
+//                 -------
+//                | •   • |
+//                |   •   |
+//                | •   • |
+//                 -------
+//                """;
+//
+//        String dice6 = """
+//                 -------
+//                | •   • |
+//                | •   • |
+//                | •   • |
+//                 -------
+//                """;
+//
+//        switch (roll){
 //            case 1 -> System.out.println(dice1);
-        }
-    }
-}
+//            case 2 -> System.out.println(dice2);
+//            case 3 -> System.out.println(dice3);
+//            case 4 -> System.out.println(dice4);
+//            case 5 -> System.out.println(dice5);
+//            case 6 -> System.out.println(dice6);
+//            default -> System.out.println("Invalid roll");
+//            case 1 -> System.out.println(dice1);
+//        }
+//    }
+//}
+
+
+
+//public class Main {
+//    public static void main(String[] args){
+//        array
+//        String[] fruits = {"apple","orange","banana","coconut"};
+//        System.out.println(fruits[0]);
+//
+//        Arrays.sort(fruits);
+//        Arrays.fill(fruits,"pineapple");
+
+//        for(int i =0; i< fruits.length; i++){
+//            System.out.println(fruits[i]);
+//        }
+//
+//        for(String fruit: fruits){
+//            System.out.println(fruit);
+//        }
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args){
+//
+//        Scanner scanner=new Scanner(System.in);
+//        String[] foods;
+//        int size;
+//
+//        System.out.println("How many foods do you want to input: ");
+//        size=scanner.nextInt();
+//        scanner.nextLine();
+//        foods=new String[size];
+//
+//        for(int i = 0; i< foods.length; i++){
+//            System.out.println("Enter a food");
+//            foods[i]=scanner.nextLine();
+//        }
+//        for(String food: foods){
+//            System.out.println(food);
+//        }
+//        scanner.close();
+//    }
+//}
+
+
+//public class Main {
+//    public static void main(String[] args){
+//        searching through an array
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int[] numbers={1,9,2,8,3,5,4};
+//        String[] fruits = {"apple","orange","pineapple"};
+//        String target;
+//        boolean isFound=false;
+//        System.out.print("What fruit are you looking for?: ");
+//        target=scanner.nextLine();
+//
+//
+//        for(int i =0; i<fruits.length; i++){
+//            if(fruits[i].equals(target)){
+//                System.out.println("Element "+fruits[i]+ " found at index "+ i);
+//                isFound=true;
+//                break;
+//            }
+//        }
+//        if(!isFound){
+//            System.out.println("Element not found in the array.");
+//        }
+//
+//        scanner.close();
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args){
+//        varargs
+//
+//        int result= add(1,3,2);
+//        double result=average(1,2,2,4);
+//        System.out.println(result);
+//    }
+//
+//    static double average(double... numbers){
+//        double sum=0;
+//        int length;
+//        length=numbers.length;
+//        if(length==0){
+//            return 0;
+//        }
+//        for(double number: numbers){
+//            sum+=number;
+//        }
+//        return sum/length;
+//    }
+//    static int add(int... numbers){
+//        System.out.println(numbers);
+//        int sum = 0;
+//        for (int number: numbers){
+//            sum+=number;
+//        }
+//        return sum;
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args){
+//        2D arrays
+
+//        String[] fruits={"apple","orange","banana"};
+//        String[] vegetables={"potato","carrot","onions"};
+//        String[] meats={"chicken","pork","beef","fish"};
+//
+//        String[][] groceries ={fruits,vegetables,meats};
+//
+//        for(String[] foods: groceries){
+//            for(String food: foods){
+//                System.out.println(food);
+//            }
+//        }
+//
+//        char[][] telephone ={
+//                {'1','2','3'},
+//                {'4','5','6'},
+//                {'7','8','9'},
+//                {'*','0','#'}
+//        };
+//
+//        for(char[] rows: telephone){
+//            for(char number: rows){
+//                System.out.print(number+" ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args){
+//        JAVA QUIZ GAME
+//        String[] questions={"What is the main function of a router?",
+//                            "Which part of the computer is considered the brain?",
+//                            "What year was facebook launched?",
+//                            "Who is known as the father of computers?",
+//                            "What was the first programming language?"
+//        };
+//
+//        String[][] options={
+//                            {"1. Storing files","2. Encrypting data","3. Directing the internet traffic","4. Managing password"},
+//                            {"1. CPU","2. Hard drive","3. RAM","4. GPU"},
+//                            {"1. 2000","2. 2004","3. 2006","4. 2008"},
+//                            {"1. Steve jobs","2. Bill Gates","3. Alan Turing","4. Charles Babbage"},
+//                            {"1. COBOL","2. C","3. FORTRAN","4. ASSEMBLY"}
+//                            };
+//
+//        int[] answers={3,1,2,4,3};
+//        int scores=0;
+//        int guess;
+//
+//        Scanner scanner= new Scanner(System.in);
+//
+//        System.out.println("*****************************");
+//        System.out.println("WELCOME TO THE JAVA QUIZ GAME");
+//        System.out.println("*****************************");
+//
+//        for(int i=0; i< questions.length;i++ ){
+//            System.out.println(questions[i]);
+//            for(String option: options[i]){
+//                System.out.println(option);
+//            }
+//            System.out.print("Enter your guess: ");
+//            guess=scanner.nextInt();
+//
+//            if(guess == answers[i]){
+//                System.out.println("********");
+//                System.out.println("CORRECT");
+//                System.out.println("*********");
+//                scores++;
+//            }else{
+//                System.out.println("********");
+//                System.out.println("WRONG!");
+//                System.out.println("*********");
+//            }
+//        }
+//        System.out.println("Your final score is "+scores);
+//        scanner.close();
+//    }
+//}
+
+//
+//public class Main {
+//    public static void main(String[] args){
+//
+//        ROCK PAPER SCISSORS GAME
+//
+//        Scanner scanner = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        String[] choices={"rock","paper","scissors"};
+//        String playerChoice;
+//        String computerChoice;
+//        String playAgain = "yes";
+//
+//
+//        do {
+//
+//            System.out.print("Enter your move (rock, scissors, paper): ");
+//            playerChoice=scanner.nextLine().toLowerCase();
+//
+//            if(!playerChoice.equals("scissors") &&
+//                    !playerChoice.equals("rock") &&
+//                    !playerChoice.equals("paper")){
+//                System.out.println("Invalid choice");
+//                continue;
+//            }
+//
+//            computerChoice=choices[random.nextInt(3)];
+//            System.out.println("Computer choice: "+ computerChoice);
+//
+//            if(playerChoice.equals(computerChoice)){
+//                System.out.println("It is a lie.");
+//            }else if((playerChoice.equals("rock") && computerChoice.equals("scissors")) ||
+//                    (playerChoice.equals("paper") && computerChoice.equals("rock")) ||
+//                    (playerChoice.equals("scissors") && computerChoice.equals("paper"))){
+//                System.out.println("You win!");
+//            } else{
+//                System.out.println("You lose.");
+//            }
+//
+//            System.out.print("Play again (yes/no): ");
+//            playAgain=scanner.nextLine();
+//        }while (playAgain.equals("yes"));
+//
+//        System.out.println("Thanks for playing.");
+//
+//        scanner.close();
+//    }
+//}
+
+//
+//public class Main {
+//    public static void main(String[] args){
+//        JAVA SLOT MACHINE
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int balance=100;
+//        int bet;
+//        int payout;
+//        String[] row;
+//        String playAgain;
+//
+//        System.out.println("***********************");
+//        System.out.println("Welcome to Java Slots");
+//        System.out.println("Symbols: 🍉 🍒 🍋 🔔 ⭐");
+//        System.out.println("***********************");
+//
+//        while (balance>0){
+//            System.out.println("Current balance: $"+ balance);
+//            System.out.print("Place your bet amount: ");
+//            bet=scanner.nextInt();
+//            scanner.nextLine();
+//
+//            if(bet > balance){
+//                System.out.println("INSUFFICIENT FUNDS");
+//                continue;
+//            } else if (bet <= 0) {
+//                System.out.println("Bet must be greater than zero");
+//            }else {
+//                balance-=bet;
+//                System.out.println("Your remaining balance "+ balance);
+//            }
+//
+//            System.out.println("Spinning...");
+//            row = spinRow();
+//            printRow(row);
+//            payout = getPayout(row,bet);
+//
+//            if(payout > 0){
+//                System.out.println("You won $"+payout);
+//                balance+=payout;
+//            }else{
+//                System.out.println("Sorry you lost this round");
+//            }
+//
+//            System.out.println("Do you want to play again? (Y/N): ");
+//            playAgain = scanner.nextLine().toUpperCase();
+//
+//            if(!playAgain.equals("Y")){
+//                break;
+//            }
+//        }
+//        System.out.println("GAME OVER! Your final balance is $"+balance);
+//
+//        scanner.close();
+//    }
+//    static String[] spinRow(){
+//        String[] symbols = {"🍉" ,"🍒", "🍋" ,"🔔", "⭐"};
+//        String[] row= new String[3];
+//        Random random=new Random();
+//
+//        System.out.println(symbols[random.nextInt(symbols.length)]);
+//        for(int i = 0; i<3; i++){
+//            row[i]=symbols[random.nextInt(symbols.length)];
+//        }
+//
+//        return row;
+//    }
+//
+//    static void printRow(String[] row){
+//        System.out.println(" "+ String.join(" | ",row));
+//    }
+//
+//    static int getPayout(String[] row, int bet){
+//        if(row[0].equals(row[1]) && row[1].equals(row[2])){
+//            return switch (row[0]){
+//                case "🍒" -> bet * 3;
+//                case "🍉" -> bet * 4;
+//                case "🍋" -> bet * 5;
+//                case "🔔" -> bet * 10;
+//                case "⭐" -> bet * 20;
+//                default -> 0;
+//            };
+//        } else if(row[0].equals(row[1])){
+//            return switch (row[0]){
+//                case "🍒" -> bet * 2;
+//                case "🍉" -> bet * 3;
+//                case "🍋" -> bet * 4;
+//                case "🔔" -> bet * 5;
+//                case "⭐" -> bet * 10;
+//                default -> 0;
+//            };
+//        }else if(row[1].equals(row[2])){
+//            return switch (row[1]){
+//                case "🍒" -> bet * 2;
+//                case "🍉" -> bet * 3;
+//                case "🍋" -> bet * 4;
+//                case "🔔" -> bet * 5;
+//                case "⭐" -> bet * 10;
+//                default -> 0;
+//            };
+//        }
+//        return 0;
+//    }
+//}
